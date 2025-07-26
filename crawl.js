9ef150686e7c6678590eb3d89d35fa3cde76b2b6
Saugat <saugatpariyar@gmail.com>
2025-07-26 15:30:22 -0500

    added check for html document in crawlPage

--- crawl.js
+++ crawl.js
@@ -1,6 +1,32 @@
 const{JSDOM} = require('jsdom') //allows us to have html syntax in node.js
 
 
+async function crawlPage(currentURL){
+    console.log(`Actively crawling: ${currentURL}`)
+    try{
+        //this resp variable contains the info about the response like status code,
+        // hearders and the body of the content
+        const resp = await fetch(currentURL) 
+        if(resp.status>399){ //checks the HTTP status code of the response
+            console.log(`error in fetch with status code: ${resp.status} on page ${currentURL}`)
+            return
+        }
+        //checking the Content-type header, and if not HTML doc, logs error and stops process
+        const contentType = resp.headers.get("content-type")
+        if(!contentType.includes("text/html")){
+            console.log(`Non-html response, content type: ${contentType} on page ${currentURL}`)
+            return
+        }
+
+        console.log(await resp.text())// the .text() method returns a promise so gotta use await
+    }catch(err){
+        console.log(`Invalid URL provided: ${err.message}`)
+    }
+}
+
+
+
+
 function getURLsFromHTML(htmlBody, baseURL){
     const urls = [];
     const dom = new JSDOM(htmlBody)
@@ -40,6 +66,7 @@ function normalizeURL(urlString){
 }
 module.exports = {
     normalizeURL,
-    getURLsFromHTML
+    getURLsFromHTML,
+    crawlPage
 
 }
--- main.js
+++ main.js
@@ -1,1 +1,16 @@
-console.log('Hello World');
+const {crawlPage} = require('./crawl.js')
+
+
+function main(){
+    if(process.argv.length<3){
+        console.log("no website provided") 
+        process.exit(1)
+    }else if(process.argv.length>3){
+        console.log("too much arguments provided")
+        process.exit(1)
+    }
+    const baseURL = process.argv[2];
+    console.log(`Starting Crawler of ${baseURL}`);
+    crawlPage(baseURL)
+}
+main()
